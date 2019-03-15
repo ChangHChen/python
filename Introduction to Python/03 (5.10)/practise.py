06789f03b30de6551420e5eab4869b934b38dee0
ChangHChen <ZhangHChen@users.noreply.github.com>
2019-03-15 17:04:18 +0900

    Create practise.py

---
+++ Introduction to Python/03 (5.10)/practise.py
@@ -0,0 +1,9 @@
+name = 'python'
+what = "programming language"
+print("{0} is {1}".format(name, what))
+print("one of {1} is {0}".format(name, what))
+print("{} is {}".format(name, what))
+
+number = 0.6
+print("{0:6.1f} is {0:.1%}". format(number))
+print("{0:.1f} is {0:7.1%}".format(number))

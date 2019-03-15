4b4c9b2c308f6ddf4fe5e064846693ad055a0f61
ChangHChen <ZhangHChen@users.noreply.github.com>
2019-03-15 17:04:52 +0900

    Create 1.py

---
+++ Introduction to Python/03 (5.10)/1.py
@@ -0,0 +1,5 @@
+colors = ["red", "grey", "blue", "yellow", "brown", "silver"]
+print("先頭は", colors[0])
+print("後ろから二番目は", colors[-2])
+for i in range(len(colors)):
+    print(i+1, "番目の色は", colors[i], "" )
